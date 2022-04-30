5cef4754d630f099fa58d8ef1b32357405060470
chenchen <cc.chen.chen@foxmail.com>
2022-04-30 22:42:53 +0800

    commit

---
+++ SpringBootDemo/src/main/java/com/ch/springbootdemo/ServerConfig.java
@@ -0,0 +1,19 @@
+package com.ch.springbootdemo;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author 渔郎
+ * @CLassName ServerConfig
+ * @Description TODO
+ * @Date 2022/4/30 22:16
+ */
+@Data
+@ConfigurationProperties(prefix = "servers")
+public class ServerConfig {
+    private String ipAddress;
+    private int port;
+    private long timeout;
+
+}
--- SpringBootDemo/src/main/java/com/ch/springbootdemo/SpringBootDemoApplication.java
+++ SpringBootDemo/src/main/java/com/ch/springbootdemo/SpringBootDemoApplication.java
@@ -1,15 +1,33 @@
 package com.ch.springbootdemo;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author 青梅君
  */
 @SpringBootApplication
+@EnableConfigurationProperties(ServerConfig.class)
 public class SpringBootDemoApplication {
+
+    @Bean
+    @ConfigurationProperties(prefix = "datasources")
+    public DruidDataSource dataSource(){
+        DruidDataSource dataSource = new DruidDataSource();
+        return dataSource;
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootDemoApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(SpringBootDemoApplication.class, args);
+        ServerConfig bean = ctx.getBean(ServerConfig.class);
+        System.out.println(bean);
+        DruidDataSource ds = ctx.getBean(DruidDataSource.class);
+        System.out.println(ds.getDriverClassName());
     }
 
 }
