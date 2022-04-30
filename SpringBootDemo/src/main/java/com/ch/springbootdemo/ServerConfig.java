b97cd9648ce10fa995ae3d63131fe87f3fae5d56
chenchen <cc.chen.chen@foxmail.com>
2022-04-30 23:34:09 +0800

    commit

--- SpringBootDemo/src/main/java/com/ch/springbootdemo/ServerConfig.java
+++ SpringBootDemo/src/main/java/com/ch/springbootdemo/ServerConfig.java
@@ -9,6 +9,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * @Description TODO
  * @Date 2022/4/30 22:16
  */
+
 @Data
 @ConfigurationProperties(prefix = "servers")
 public class ServerConfig {
