13f9a4c7d5ed75026eb59cfc266092dd11a322f9
chenchen <cc.chen.chen@foxmail.com>
2022-04-30 19:29:17 +0800

    commit

--- springboot_ssmp/src/main/java/com/ch/controller/IBookController.java
+++ springboot_ssmp/src/main/java/com/ch/controller/IBookController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/books")
 public class IBookController {
+
     private IBookService iBookService;
     @Autowired
     public void setiBookService(IBookService iBookService) {
