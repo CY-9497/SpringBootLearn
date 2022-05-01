b18cc852186d653eac8dfe58e509258f211f83c8
CY-9497 <cc.chen.chen@foxmail.com>
2022-05-01 22:21:54 +0800

    commit

--- src/main/java/com/ch/HelloApplication.java
+++ src/main/java/com/ch/HelloApplication.java
@@ -8,13 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @CLassName com.ch.HelloApplication
  * @Description TODO
  * @Date 2022/4/15 11:49
- *
+ * <p>
  * 引导类  SpringBoot的入口
  * 与controller放在同一个包内，这是硬性要求
  */
 @SpringBootApplication
 public class HelloApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HelloApplication.class,args);
+        SpringApplication.run(HelloApplication.class, args);
     }
 }
